37b120f7478d54756631d2874933ac442eed2df4
Pedro Ives <contact.pedroives@gmail.com>
2024-09-11 19:13:25 -0300

    feat: Add features -> for/while

---
+++ IsiLanguage/src/io/compiler/ast/CommandLoopFor.java
@@ -0,0 +1,32 @@
+package io.compiler.ast;
+
+import java.util.ArrayList;
+
+public class CommandLoopFor extends AbstractCommand {
+	
+	private String condition;
+	private ArrayList<AbstractCommand> cmdList;
+	
+	public CommandLoopFor(String condition, ArrayList<AbstractCommand> cmdList) {
+		this.condition = condition;
+		this.cmdList = cmdList;
+	}
+	
+	@Override
+	public String generateJavaCode() {
+		StringBuilder str = new StringBuilder();
+		
+		str.append("    for (" + this.condition + ") {\n");
+		for (AbstractCommand cmd: cmdList) {
+			str.append("    " + cmd.generateJavaCode());
+		}
+		str.append("    }\n");
+		
+		return str.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("CommandLoopFor [condition=%s, cmdList=%s]", this.condition, this.cmdList);
+	}
+}
---
+++ IsiLanguage/src/io/compiler/ast/CommandLoopWhile.java
@@ -0,0 +1,29 @@
+package io.compiler.ast;
+
+import java.util.ArrayList;
+
+public class CommandLoopWhile extends AbstractCommand {
+    private String condition;
+    private ArrayList<AbstractCommand> cmdList;
+
+    public CommandLoopWhile(String condition, ArrayList<AbstractCommand> cmdList) {
+        this.condition = condition;
+        this.cmdList = cmdList;
+    }
+
+    @Override
+    public String generateJavaCode() {
+        StringBuilder str = new StringBuilder();
+        str.append("    while (" + condition + ") {\n");
+        for (AbstractCommand cmd : cmdList) {
+            str.append("    " + cmd.generateJavaCode());
+        }
+        str.append("    }\n");
+        return str.toString();
+    }
+    
+    @Override
+	public String toString() {
+		return String.format("CommandLoopWhile [condition=%s, cmdList=%s]", this.condition, this.cmdList);
+	}
+}
